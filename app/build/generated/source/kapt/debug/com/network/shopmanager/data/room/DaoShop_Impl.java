3a859e498078d49b23f44be48e286e7074eda1af
U <umtokh80@gmail.com>
2022-11-28 00:57:23 +0500

    Retroit, Room, RxJava lar qoshildi. Kurs valuta ishlayapti

---
+++ app/build/generated/source/kapt/debug/com/network/shopmanager/data/room/DaoShop_Impl.java
@@ -0,0 +1,376 @@
+package com.network.shopmanager.data.room;
+
+import android.database.Cursor;
+import androidx.room.EntityDeletionOrUpdateAdapter;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.RxRoom;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import com.network.shopmanager.data.models.Shop;
+import io.reactivex.Flowable;
+import io.reactivex.Single;
+import java.lang.Class;
+import java.lang.Exception;
+import java.lang.Integer;
+import java.lang.Long;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class DaoShop_Impl implements DaoShop {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<Shop> __insertionAdapterOfShop;
+
+  private final EntityDeletionOrUpdateAdapter<Shop> __deletionAdapterOfShop;
+
+  private final EntityDeletionOrUpdateAdapter<Shop> __updateAdapterOfShop;
+
+  public DaoShop_Impl(RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfShop = new EntityInsertionAdapter<Shop>(__db) {
+      @Override
+      public String createQuery() {
+        return "INSERT OR REPLACE INTO `shops` (`id`,`name`,`address`,`geo`,`photo`,`date`) VALUES (?,?,?,?,?,?)";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, Shop value) {
+        if (value.getId() == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindString(1, value.getId());
+        }
+        if (value.getName() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getName());
+        }
+        if (value.getAddress() == null) {
+          stmt.bindNull(3);
+        } else {
+          stmt.bindString(3, value.getAddress());
+        }
+        if (value.getGeo() == null) {
+          stmt.bindNull(4);
+        } else {
+          stmt.bindString(4, value.getGeo());
+        }
+        if (value.getPhoto() == null) {
+          stmt.bindNull(5);
+        } else {
+          stmt.bindString(5, value.getPhoto());
+        }
+        stmt.bindLong(6, value.getDate());
+      }
+    };
+    this.__deletionAdapterOfShop = new EntityDeletionOrUpdateAdapter<Shop>(__db) {
+      @Override
+      public String createQuery() {
+        return "DELETE FROM `shops` WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, Shop value) {
+        if (value.getId() == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindString(1, value.getId());
+        }
+      }
+    };
+    this.__updateAdapterOfShop = new EntityDeletionOrUpdateAdapter<Shop>(__db) {
+      @Override
+      public String createQuery() {
+        return "UPDATE OR ABORT `shops` SET `id` = ?,`name` = ?,`address` = ?,`geo` = ?,`photo` = ?,`date` = ? WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, Shop value) {
+        if (value.getId() == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindString(1, value.getId());
+        }
+        if (value.getName() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getName());
+        }
+        if (value.getAddress() == null) {
+          stmt.bindNull(3);
+        } else {
+          stmt.bindString(3, value.getAddress());
+        }
+        if (value.getGeo() == null) {
+          stmt.bindNull(4);
+        } else {
+          stmt.bindString(4, value.getGeo());
+        }
+        if (value.getPhoto() == null) {
+          stmt.bindNull(5);
+        } else {
+          stmt.bindString(5, value.getPhoto());
+        }
+        stmt.bindLong(6, value.getDate());
+        if (value.getId() == null) {
+          stmt.bindNull(7);
+        } else {
+          stmt.bindString(7, value.getId());
+        }
+      }
+    };
+  }
+
+  @Override
+  public Single<Long> addShop(final Shop shop) {
+    return Single.fromCallable(new Callable<Long>() {
+      @Override
+      public Long call() throws Exception {
+        __db.beginTransaction();
+        try {
+          long _result = __insertionAdapterOfShop.insertAndReturnId(shop);
+          __db.setTransactionSuccessful();
+          return _result;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    });
+  }
+
+  @Override
+  public Single<List<Long>> addShops(final List<Shop> shops) {
+    return Single.fromCallable(new Callable<List<Long>>() {
+      @Override
+      public List<Long> call() throws Exception {
+        __db.beginTransaction();
+        try {
+          List<Long> _result = __insertionAdapterOfShop.insertAndReturnIdsList(shops);
+          __db.setTransactionSuccessful();
+          return _result;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    });
+  }
+
+  @Override
+  public Single<Integer> deleteShop(final Shop shop) {
+    return Single.fromCallable(new Callable<Integer>() {
+      @Override
+      public Integer call() throws Exception {
+        int _total = 0;
+        __db.beginTransaction();
+        try {
+          _total +=__deletionAdapterOfShop.handle(shop);
+          __db.setTransactionSuccessful();
+          return _total;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    });
+  }
+
+  @Override
+  public Single<Integer> updateShop(final Shop shop) {
+    return Single.fromCallable(new Callable<Integer>() {
+      @Override
+      public Integer call() throws Exception {
+        int _total = 0;
+        __db.beginTransaction();
+        try {
+          _total +=__updateAdapterOfShop.handle(shop);
+          __db.setTransactionSuccessful();
+          return _total;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    });
+  }
+
+  @Override
+  public Flowable<List<Shop>> getShops() {
+    final String _sql = "select * from shops";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    return RxRoom.createFlowable(__db, false, new String[]{"shops"}, new Callable<List<Shop>>() {
+      @Override
+      public List<Shop> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfName = CursorUtil.getColumnIndexOrThrow(_cursor, "name");
+          final int _cursorIndexOfAddress = CursorUtil.getColumnIndexOrThrow(_cursor, "address");
+          final int _cursorIndexOfGeo = CursorUtil.getColumnIndexOrThrow(_cursor, "geo");
+          final int _cursorIndexOfPhoto = CursorUtil.getColumnIndexOrThrow(_cursor, "photo");
+          final int _cursorIndexOfDate = CursorUtil.getColumnIndexOrThrow(_cursor, "date");
+          final List<Shop> _result = new ArrayList<Shop>(_cursor.getCount());
+          while(_cursor.moveToNext()) {
+            final Shop _item;
+            final String _tmpId;
+            if (_cursor.isNull(_cursorIndexOfId)) {
+              _tmpId = null;
+            } else {
+              _tmpId = _cursor.getString(_cursorIndexOfId);
+            }
+            final String _tmpName;
+            if (_cursor.isNull(_cursorIndexOfName)) {
+              _tmpName = null;
+            } else {
+              _tmpName = _cursor.getString(_cursorIndexOfName);
+            }
+            final String _tmpAddress;
+            if (_cursor.isNull(_cursorIndexOfAddress)) {
+              _tmpAddress = null;
+            } else {
+              _tmpAddress = _cursor.getString(_cursorIndexOfAddress);
+            }
+            final String _tmpGeo;
+            if (_cursor.isNull(_cursorIndexOfGeo)) {
+              _tmpGeo = null;
+            } else {
+              _tmpGeo = _cursor.getString(_cursorIndexOfGeo);
+            }
+            final String _tmpPhoto;
+            if (_cursor.isNull(_cursorIndexOfPhoto)) {
+              _tmpPhoto = null;
+            } else {
+              _tmpPhoto = _cursor.getString(_cursorIndexOfPhoto);
+            }
+            final long _tmpDate;
+            _tmpDate = _cursor.getLong(_cursorIndexOfDate);
+            _item = new Shop(_tmpId,_tmpName,_tmpAddress,_tmpGeo,_tmpPhoto,_tmpDate);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+
+  @Override
+  public long getShopsLastDate() {
+    final String _sql = "select date from shops order by date desc limit 1";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    __db.assertNotSuspendingTransaction();
+    final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+    try {
+      final long _result;
+      if(_cursor.moveToFirst()) {
+        _result = _cursor.getLong(0);
+      } else {
+        _result = 0L;
+      }
+      return _result;
+    } finally {
+      _cursor.close();
+      _statement.release();
+    }
+  }
+
+  @Override
+  public int getShopsCount() {
+    final String _sql = "select count(*) from shops";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    __db.assertNotSuspendingTransaction();
+    final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+    try {
+      final int _result;
+      if(_cursor.moveToFirst()) {
+        _result = _cursor.getInt(0);
+      } else {
+        _result = 0;
+      }
+      return _result;
+    } finally {
+      _cursor.close();
+      _statement.release();
+    }
+  }
+
+  @Override
+  public Shop getShopById(final String id) {
+    final String _sql = "select * from shops where id=?";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 1);
+    int _argIndex = 1;
+    if (id == null) {
+      _statement.bindNull(_argIndex);
+    } else {
+      _statement.bindString(_argIndex, id);
+    }
+    __db.assertNotSuspendingTransaction();
+    final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+    try {
+      final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+      final int _cursorIndexOfName = CursorUtil.getColumnIndexOrThrow(_cursor, "name");
+      final int _cursorIndexOfAddress = CursorUtil.getColumnIndexOrThrow(_cursor, "address");
+      final int _cursorIndexOfGeo = CursorUtil.getColumnIndexOrThrow(_cursor, "geo");
+      final int _cursorIndexOfPhoto = CursorUtil.getColumnIndexOrThrow(_cursor, "photo");
+      final int _cursorIndexOfDate = CursorUtil.getColumnIndexOrThrow(_cursor, "date");
+      final Shop _result;
+      if(_cursor.moveToFirst()) {
+        final String _tmpId;
+        if (_cursor.isNull(_cursorIndexOfId)) {
+          _tmpId = null;
+        } else {
+          _tmpId = _cursor.getString(_cursorIndexOfId);
+        }
+        final String _tmpName;
+        if (_cursor.isNull(_cursorIndexOfName)) {
+          _tmpName = null;
+        } else {
+          _tmpName = _cursor.getString(_cursorIndexOfName);
+        }
+        final String _tmpAddress;
+        if (_cursor.isNull(_cursorIndexOfAddress)) {
+          _tmpAddress = null;
+        } else {
+          _tmpAddress = _cursor.getString(_cursorIndexOfAddress);
+        }
+        final String _tmpGeo;
+        if (_cursor.isNull(_cursorIndexOfGeo)) {
+          _tmpGeo = null;
+        } else {
+          _tmpGeo = _cursor.getString(_cursorIndexOfGeo);
+        }
+        final String _tmpPhoto;
+        if (_cursor.isNull(_cursorIndexOfPhoto)) {
+          _tmpPhoto = null;
+        } else {
+          _tmpPhoto = _cursor.getString(_cursorIndexOfPhoto);
+        }
+        final long _tmpDate;
+        _tmpDate = _cursor.getLong(_cursorIndexOfDate);
+        _result = new Shop(_tmpId,_tmpName,_tmpAddress,_tmpGeo,_tmpPhoto,_tmpDate);
+      } else {
+        _result = null;
+      }
+      return _result;
+    } finally {
+      _cursor.close();
+      _statement.release();
+    }
+  }
+
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
